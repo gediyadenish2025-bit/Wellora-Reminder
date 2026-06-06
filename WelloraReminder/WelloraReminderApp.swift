918bd7ccb598e9e36aaa1c5099a1deaff1ac0ad6
Denish Gediya <>
2026-06-06 10:21:32 +0530

    Project setup done

---
+++ WelloraReminder/WelloraReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  WelloraReminderApp.swift
+//  WelloraReminder
+//
+//  Created by Denish Gediya on 06/06/26.
+//
+
+import SwiftUI
+
+@main
+struct WelloraReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
